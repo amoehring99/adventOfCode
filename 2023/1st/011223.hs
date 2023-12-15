629f122a60f4b3f4b6f742a7b7270f28a5d9d44e
Alexander Moehring <github@amoehring.com>
2023-12-15 15:15:51 +0100

    added solution for first advent second part of code task in haskell

--- 2023/1st/011223.hs
+++ 2023/1st/011223.hs
@@ -1,4 +1,5 @@
 import Data.Char (isDigit)
+import Data.Text (Text, pack, replace, unpack)
 
 concatFirstAndLastDigit :: String -> String
 concatFirstAndLastDigit [] = []
@@ -8,6 +9,23 @@ concatFirstAndLastDigit (x : xs) = x : [last xs]
 sumOfDigits :: [String] -> Int
 sumOfDigits = foldr ((+) . read) 0
 
+replaceWrittenDigitWithDigit :: [String] -> [String]
+replaceWrittenDigitWithDigit =
+  map
+    ( unpack
+        . replace (pack "zero") (pack "z0o")
+        . replace (pack "one") (pack "o1e")
+        . replace (pack "two") (pack "t2o")
+        . replace (pack "three") (pack "t3e")
+        . replace (pack "four") (pack "f4r")
+        . replace (pack "five") (pack "f5e")
+        . replace (pack "six") (pack "s6x")
+        . replace (pack "seven") (pack "s7n")
+        . replace (pack "eight") (pack "e8t")
+        . replace (pack "nine") (pack "n9e")
+        . pack
+    )
+
 main = do
   contents <- readFile "/home/amoehring99/git/adventOfCode/2023/1st/puzzleInput.txt"
-  print . sumOfDigits . map (concatFirstAndLastDigit . filter isDigit) . words $ contents
+  print . sumOfDigits . map (concatFirstAndLastDigit . filter isDigit) . replaceWrittenDigitWithDigit . words $ contents
